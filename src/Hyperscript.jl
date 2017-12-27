9b9bae28d19144d2c5cb1a9b49f3ae7cc70be679
Yuri Vishnevsky <yurivish@gmail.com>
2017-12-27 14:07:04 -0500

    Add initial contents: code, validation jsons

--- src/Hyperscript.jl
+++ src/Hyperscript.jl
@@ -1,5 +1,254 @@
+__precompile__()
+
 module Hyperscript
 
-# package code goes here
+export m, @tags
+
+include("constants.jl")
+
+# To reduce redundancy, we create some constant values here
+const COMBINED_ATTRS = merge(unique∘vcat, SVG_ATTRS, HTML_ATTRS)
+const COMBINED_TAGS = union(SVG_TAGS, HTML_TAGS)
+const COMBINED_ATTR_NAMES = merge(HTML_ATTR_NAME, SVG_ATTR_NAME)
+
+isvoid(tag) = tag in COMBINED_VOID_TAGS
+
+## Validation types and implementation
+
+"""
+Validations provide checks against common typos and mistakes. They are non-exhaustive
+and enforce valid HTML/SVG tag names as well as enforcing that the passed attributes
+are allowed on the given tag, e.g. you can use `cx` but not `x` on a `<circle />`.
+
+Validations other than `ValidateNone` also enforce that numeric attribute values are non-`NaN`.
+
+The specific tag and attribute names depend on the chosen validation. The default is
+*combined* validation via `ValidateCombined`, which liberally accepts any mix of valid
+HTML/SVG tags and attributes.
+
+Note that `ValidateCombined` does not enforce inter-attribute consistency. If you use a tag
+that belongs to both SVG and HTML, it will accept any mix of HTML and SVG attributes for
+that tag even when the valid attributes for that HTML tag and SVG tag differ.
+
+These are all of the tags shared between HTML and SVG:
+
+$HTML_SVG_TAG_INTERSECTION_MD
+"""
+abstract type Validation end
+
+"Validates generously against the combination of HTML and SVG."
+struct ValidateCombined <: Validation end
+
+"Validates generously against the combination of SVG 1.1, SVG Tiny 1.2, and SVG 2."
+struct ValidateSVG <: Validation end
+
+"Validates generously against the combination of HTML 4, W3C HTML 5, and WHATWG HTML 5."
+struct ValidateHTML <: Validation end
+
+"Does not validate input tag names, attribute names, or attribute values."
+struct ValidateNone <: Validation end
+
+sym2code(::ValidateCombined) = COMBINED_ATTR_NAMES
+sym2code(::ValidateHTML) = HTML_ATTR_NAME
+sym2code(::ValidateSVG) = SVG_ATTR_NAME
+
+tag2attrs(::ValidateCombined) = COMBINED_ATTRS
+tag2attrs(::ValidateHTML) = HTML_ATTRS
+tag2attrs(::ValidateSVG) = SVG_ATTRS
+
+tags(::ValidateCombined) = COMBINED_TAGS
+tags(::ValidateHTML) = HTML_TAGS
+tags(::ValidateSVG) = SVG_TAGS
+
+name(::ValidateCombined) = "HTML or SVG"
+name(::ValidateHTML) = "HTML"
+name(::ValidateSVG) = "SVG"
+
+function validatetag(v::Validation, tag)
+    tag ∈ tags(v) || error("$tag is not a valid $(name(v)) tag")
+    tag
+end
+validatetag(v::ValidateNone, tag) = tag
+
+function validatevalue(v::Validation, tag, attr, value::Number)
+    # Technically a NaN is valid attribute value, but we consider it
+    # an error since it's almost never what you actually want.
+    # TODO: Find some way to disable this check; somebody somewhere will
+    # probably want NaNs to pass through.
+    isnan(value) && error("A NaN value was passed to an attribute: $(stringify(tag, attr, value))")
+    value
+end
+validatevalue(v, tag, attr, value) = value
+validatevalue(v::ValidateNone, tag, attr, value) = value
+
+function validateattrs(v::Validation, tag, nt::NamedTuple)
+    sym_to_code = sym2code(v)
+    ATTRS = tag2attrs(v)
+    attrs = Dict{String, Any}()
+    for (sym, value) in pairs(nt)
+        attr = get(sym_to_code, sym) do
+            error("$(string(sym)) is not a valid attribute name: $(stringify(tag, sym, value))")
+        end
+        validatevalue(v, tag, attr, value)
+        valid = attr ∈ ATTRS[tag] || attr ∈ ATTRS["*"]
+        valid || error("$attr is not a valid attribute name for $tag tags")
+        attrs[attr] = value
+    end
+    attrs
+end
+
+function validateattrs(v::ValidateNone, tag, nt::NamedTuple)
+    Dict{String, Any}(string(sym) => value for (sym, value) in pairs(nt))
+end
+
+##################
+
+
+# For nicer printing in errors
+stringify(tag) = string("<", tag, isvoid(tag) ? " />" : ">")
+stringify(tag, attr, value) = string("<", tag, " ", attr, "=\"", value, "\"", isvoid(tag) ? " />" : ">")
+
+## Node representation and generation
+
+struct Node{V<:Validation}
+    tag::String
+    attrs::Dict{String, Any}
+    children::Vector{Any}
+end
+
+function Node(v::V, tag, children, attrs) where {V <: Validation}
+    Node{V}(validatetag(v, tag), validateattrs(v, tag, attrs), flat(children))
+end
+
+tag(x::Node) = Base.getfield(x, :tag)
+attrs(x::Node) = Base.getfield(x, :attrs)
+children(x::Node) = Base.getfield(x, :children)
+
+# Allow extending a node using function application syntax.
+# Overrides attributes and appends children.
+function (node::Node{V})(cs...; as...) where {V <: Validation}
+    Node{V}(
+        tag(node),
+        isempty(as) ? attrs(node)    : merge(attrs(node), validateattrs(V(), tag(node), as)),
+        isempty(cs) ? children(node) : prepend!(flat(cs), children(node))
+    )
+end
+
+# Recursively flatten generators, tuples, and arrays.
+# Wraps scalars in a single-element tuple.
+# Note: We could do something trait-based, so custom lazy collections can opt into compatibility
+function flat(xs::Union{Base.Generator, Tuple, Array})
+    out = []
+    for x in xs
+        append!(out, flat(x))
+    end
+    out
+end
+flat(x) = (x,)
+
+# Allow concise class attribute specification.
+# Classes specified this way will append to an existing class if present.
+function Base.getproperty(x::Node, class::Symbol)
+    a = attrs(x)
+    x(class=haskey(a, "class") ? string(a["class"], " ", class) : string(class))
+end
+
+"""
+`m(tag, children...; attrs)`
+
+Create a hypertext node with the specified attributes and children. `m` performs
+validation against SVG and HTML tags and attributes; use `m_svg`, `m_html` to
+validate against just SVG or HTML, or use `m_novalidate` to prevent validation
+entirely.
+
+The following import pattern is useful for convenient access to your choice
+of validation style:
+
+```julia
+import Hyperscript
+const m = Hyperscript.m_svg
+```
+
+The `children` can be any Julia values, including other `Node`s creates by `m`.
+Tuples, arrays, and generators will be recursively flattened.
+
+Since attribute names are passed as Julia symbols `m(attrname=value)`, Hyperscript
+accepts both Julia-style (lowercase) and JSX-like (camelCase) attributes:
+
+`acceptCharset` turns into the HTML attribute `accept-charset`, as does `acceptcharset`.
+"""
+m(tag, children...; attrs...)            = Node(ValidateCombined(), tag, children, attrs)
+m_svg(tag, children...; attrs...)        = Node(ValidateSVG(), tag, children, attrs)
+m_html(tag, children...; attrs...)       = Node(ValidateHTML(), tag, children, attrs)
+m_novalidate(tag, children...; attrs...) = Node(ValidateNone(), tag, children, attrs)
+
+"""
+Macro for concisely declaring a number of tags in global scope.
+
+`@tags h1 h2 span` expands into
+
+```
+const h1 = m("h1")
+const h2 = m("h2")
+const span = m("span")
+```
+
+The `const` declaration precludes this macro from being used in
+non-global scopes (e.g. inside a function) since const is disallowed
+on local variables. It is present for performance.
+"""
+macro tags(args::Symbol...)
+    blk = Expr(:block)
+    for tag in args
+        push!(blk.args, quote
+            const $(esc(tag)) = m($(string(tag)))
+        end)
+    end
+    push!(blk.args, nothing)
+    blk
+end
+
+## Markup generation
+
+# Creates an HTML escaping dictionary
+chardict(chars) = Dict(c => "&#$(Int(c));" for c in chars)
+# See: https://stackoverflow.com/questions/7753448/how-do-i-escape-quotes-in-html-attribute-values
+const ATTR_ESCAPES = chardict("&<>\"\n\r\t")
+# See: https://stackoverflow.com/a/9189067/1175713
+const HTML_ESCAPES = chardict("&<>\"'`!@\$%()=+{}[]")
+
+printescaped(io, x, replacements=HTML_ESCAPES) = for c in x
+    print(io, get(replacements, c, c))
+end
+
+function render(io::IO, x)
+    mime = MIME(mimewritable(MIME("text/html"), x) ? "text/html" : "text/plain")
+    printescaped(io, sprint(show, mime, x))
+end
+
+render(io::IO, x::Union{AbstractString, Char}) = printescaped(io, x)
+render(io::IO, x::Number) = printescaped(io, string(x))
+render(node::Node) = sprint(render, node)
+function render(io::IO, node::Node)
+    print(io, "<", tag(node))
+    for (k, v) in pairs(attrs(node))
+        print(io, " ", k, "=\"")
+        printescaped(io, v, ATTR_ESCAPES)
+        print(io, "\"")
+    end
+    if isvoid(tag(node))
+        @assert isempty(children(node))
+        print(io, " />")
+    else
+        print(io, ">")
+        for child in children(node)
+            render(io, child)
+        end
+        print(io, "</", tag(node), ">")
+    end
+end
+
+Base.show(io::IO, ::MIME"text/html",  node::Node) = render(io, node)
+Base.show(io::IO, node::Node) = render(io, node)
 
 end # module
---
+++ src/generate.jl
@@ -0,0 +1,125 @@
+using Unicode # for attr2symbols
+using Base.Iterators, JSON # for data generation
+
+load(fname) = JSON.parse(read(fname, String); dicttype=Dict{String, Vector{String}})
+
+## Validation data
+
+"""
+Map of SVG elements to allowed attributes. Also contains global attributes under '*'.
+Includes attributes from SVG 1.1, SVG Tiny 1.2, and SVG 2.
+Note: Does not include ARIA attributes (role, aria-*), xml:* or xlink:* attributes, event attributes (on*), or ev:event
+[Source](https://github.com/wooorm/svg-element-attributes/blob/2b028fecbf10df35162e63ee50308138068331a6/index.json)
+"""
+const SVG_ATTRS = load("svg.json")
+
+"""
+List of known SVG tag-names. Includes the elements from SVG 1.1, SVG Tiny 1.2, and SVG 2.
+[Source](https://github.com/wooorm/svg-tag-names/blob/a30d82c127d9959add5c357e44f5822c15eb8540/index.json)
+"""
+const SVG_TAGS = Set{String}(load("svgtags.json"))
+
+"""
+Map of HTML elements to allowed attributes. Also contains global attributes under '*'. Includes attributes from HTML 4, W3C HTML 5, and WHATWG HTML 5.
+Note: Includes deprecated attributes.
+Note: Attributes which were not global in HTML 4 but are in HTML 5, are only included in the list of global attributes.
+[Source](https://github.com/wooorm/html-element-attributes/blob/2d4db7c929552c35e2720a8d99547da72f8dde52/index.json)
+"""
+const HTML_ATTRS = load("html.json")
+
+"""
+List of known HTML tag-names. Includes ancient (for example, nextid and basefont) and modern (for example, shadow and template) tag-names from both W3C and WHATWG.
+[Source](https://github.com/wooorm/html-tag-names/blob/ef96f74a78b4fbe343518a6c156692e12446987a/index.json)
+"""
+const HTML_TAGS = Set{String}(load("htmltags.json"))
+
+"""
+Human-readable list of tags that are both HTML and SVG.
+Destined for a docstring.
+"""
+const HTML_SVG_TAG_INTERSECTION_MD = join(["`$tag`" for tag in intersect(SVG_TAGS, HTML_TAGS)] , ", ", ", and ")
+
+"""
+List of attributes defined by [ARIA](https://www.w3.org/TR/aria-in-html/).
+[Source](https://github.com/wooorm/aria-attributes/blob/b5dc0dfb1a97ed89eee6b3229527f240db054754/index.json)
+"""
+const ARIA_EXTRAS = load("aria.json")
+
+# Section M.2: https://www.w3.org/TR/SVG/attindex.html
+const SVG_PRESENTATION_ATTRIBUTES = ["alignment-baseline", "baseline-shift", "clip-path", "clip-rule", "clip", "color-interpolation-filters", "color-interpolation", "color-profile", "color-rendering", "color", "cursor", "direction", "display", "dominant-baseline", "enable-background", "fill-opacity", "fill-rule", "fill", "filter", "flood-color", "flood-opacity", "font-family", "font-size-adjust", "font-size", "font-stretch", "font-style", "font-variant", "font-weight", "glyph-orientation-horizontal", "glyph-orientation-vertical", "image-rendering", "kerning", "letter-spacing", "lighting-color", "marker-end", "marker-mid", "marker-start", "mask", "opacity", "overflow", "pointer-events", "shape-rendering", "stop-color", "stop-opacity", "stroke-dasharray", "stroke-dashoffset", "stroke-linecap", "stroke-linejoin", "stroke-miterlimit", "stroke-opacity", "stroke-width", "stroke", "text-anchor", "text-decoration", "text-rendering", "unicode-bidi", "visibility", "word-spacing", "writing-mode"]
+const SVG_PRESENTATION_ELEMENTS   = ["a", "altGlyph", "animate", "animateColor", "circle", "clipPath", "defs", "ellipse", "feBlend", "feColorMatrix", "feComponentTransfer", "feComposite", "feConvolveMatrix", "feDiffuseLighting", "feDisplacementMap", "feFlood", "feGaussianBlur", "feImage", "feMerge", "feMorphology", "feOffset", "feSpecularLighting", "feTile", "feTurbulence", "filter", "font", "foreignObject", "g", "glyph", "glyphRef", "image", "line", "linearGradient", "marker", "mask", "missing-glyph", "path", "pattern", "polygon", "polyline", "radialGradient", "rect", "stop", "svg", "switch", "symbol", "text", "textPath", "tref", "tspan", "use"]
+for tag in SVG_PRESENTATION_ELEMENTS
+    append!(SVG_ATTRS[tag], SVG_PRESENTATION_ATTRIBUTES)
+end
+
+
+# Allow all ARIA attributes on all HTML and SVG elements.
+# This is over-generous.
+append!(SVG_ATTRS["*"], ARIA_EXTRAS)
+append!(HTML_ATTRS["*"], ARIA_EXTRAS)
+
+# Used to validate by default.
+# Allows mixing of HTML and SVG attributes on the same element.
+const COMBINED_ATTRS = merge(unique∘vcat, SVG_ATTRS, HTML_ATTRS)
+const COMBINED_TAGS = union(SVG_TAGS, HTML_TAGS)
+
+function attr2symbols(attr)
+    Symbol.(if contains(attr, '-')
+        pieces = split(attr, '-')
+        (join(pieces), join([first(pieces), map(ucfirst, pieces[2:end])...]))
+    else
+        if any(isupper, attr)
+            (lowercase(attr), attr)
+        else
+            (attr,)
+        end
+    end)
+end
+
+function sym_to_attr_dict(attrs)
+    Dict{Symbol, String}(sym => attr for attr in unique(flatten(values(attrs))) for sym in attr2symbols(attr))
+end
+
+# Lookup tables from kwargs symbols to attribute strings.
+# e.g. :viewbox => "viewBox", :viewBox => "viewBox"
+# e.g. :stopcolor => "stop-color", :stopColor => "stop-color"
+const HTML_ATTR_NAME = sym_to_attr_dict(HTML_ATTRS)
+const SVG_ATTR_NAME = sym_to_attr_dict(SVG_ATTRS)
+const COMBINED_ATTR_NAME = merge(HTML_ATTR_NAME, SVG_ATTR_NAME)
+
+# Void elements are not allowed to contain content
+# See: http://www.w3.org/TR/html5/syntax.html#void-elements
+const HTML_VOID_TAGS = Set{String}(["area", "base", "br", "col", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr"])
+# See: https://github.com/jonschlinkert/self-closing-tags
+const SVG_VOID_TAGS = Set{String}(["circle", "ellipse", "line", "path", "polygon", "polyline", "rect", "stop", "use"])
+const COMBINED_VOID_TAGS = union(SVG_VOID_TAGS, HTML_VOID_TAGS)
+
+# Guard against the unlikely chance that a tag is not void in both
+# HTML and SVG in a future version of either spec; this would invalidate
+# the assumptin made by `isvoid`.
+@assert all(tag -> !(tag ∈ SVG_VOID_TAGS && tag ∈ HTML_VOID_TAGS), COMBINED_VOID_TAGS)
+
+macro vardecl(x)
+    name = string(x)
+    quote
+        string("const ", $name, " = ", sprint(showcompact, $x))
+    end
+end
+
+open("constants.jl", "w") do io
+    println(io, "# This code was generated with generate.jl")
+    println(io, @vardecl(HTML_TAGS))
+    println(io, @vardecl(SVG_TAGS))
+    # println(io, @vardecl(COMBINED_TAGS))
+
+    println(io, @vardecl(HTML_ATTRS))
+    println(io, @vardecl(SVG_ATTRS))
+    # println(io, @vardecl(COMBINED_ATTRS))
+
+    println(io, @vardecl(HTML_ATTR_NAME))
+    println(io, @vardecl(SVG_ATTR_NAME))
+    # println(io, @vardecl(COMBINED_ATTR_NAME))
+
+    println(io, @vardecl(COMBINED_VOID_TAGS))
+    println(io, @vardecl(HTML_SVG_TAG_INTERSECTION_MD))
+end
