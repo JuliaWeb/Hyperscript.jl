c81be2aba28faefaaf48ffa3c6f712de0100984a
Yuri Vishnevsky <yurivish@gmail.com>
2018-02-22 15:53:54 -0500

    new units iteration
    Former-commit-id: 89ca4522b5d948d94213650072988a13c6293205

--- src/units.jl
+++ src/units.jl
@@ -1,53 +1,19 @@
-# Fooling around. Does not work in the current state.
-
-abstract type Unit{T<:Number} end
-
-# convenient concise scalar * unit constructor, e.g. 5px
-struct BareUnit{T} end
-Base.:*(x::Number, ::BareUnit{T}) where {T} = Unit(x)
-
-struct Unit{suffix, T} <: Unit{T}
+struct Unit{S, T} # S = suffix symbol
     value::T
 end
-Base.show(io::IO, x::Unit{suffix}) where {suffix} = print(io, x.value, suffix)
+Unit{S}(value) where {S} = Unit{S, typeof(value)}(value)
 
-const px = BareUnit{Unit}()
+Base.show(io::IO, x::Unit{S}) where {S} = print(io, x.value, S)
 
-
-# macro unit(typ, shortname, suffix)
-#     #=
-#     struct Px{T} <: Unit{T}
-#         value::T
-#     end
-#     const px = BareUnit{Px}()
-#     baseunit(::Type{T}) where {T <: Px} = Px
-#     Base.show(io::IO, x::Px) = print(io, x.value, "px")
-#     =#
-#     quote
-#         struct $typ{T} <: Unit{T}
-#             value::T
-#         end
-#         const $(esc(shortname)) = BareUnit{$typ}()
-#         $(esc(:baseunit))(::Type{T}) where {T <: $typ} = $typ
-#         Base.show(io::IO, x::$typ) = print(io, x.value, $suffix)
-#     end
-# end
-
-# # common css units (ex, ch excluded)
-# @unit Px px "px"
-# @unit Em em "em"
-# @unit Rem rem "rem"
-# @unit Pc pc "%"
-# @unit Vh vh "vh"
-# @unit Vw vw "vw"
-# @unit Vmin vmin "vmin"
-# @unit Vmax vmax "vmax"
-
-# scalar * unit
-Base.:*(x::Number, y::U) where {U <: Unit} = baseunit(U)(x * y.value)
+# concise scalar * unit construction, e.g. 5px
+struct BareUnit{T} end
+Base.:*(x::T, ::BareUnit{U}) where {U <: Unit, T<:Number} = U{T}(x)
 
 # diagonal dispatch for unit + unit
-Base.:+(x::U, y::U) where {U <: Unit} = baseunit(U)(x.value + y.value)
+Base.:+(x::Unit{S}, y::Unit{S}) where {S} = Unit{S}(x.value + y.value)
+
+# scalar * unit
+Base.:*(x::Number, y::U) where {U <: Unit} = U(x * y.value)
 
 # calc() expressions
 struct Calc
@@ -56,7 +22,7 @@ struct Calc
 end
 Base.show(io::IO, x::Calc) = print(io, "calc", x.expr)
 
-# fallback to calc() for mismatched units
+# default to calc() for mismatched units
 Base.:+(x::Unit,   y::Unit) = Calc("$x + $y")
 
 # scalar * calc(), unit + calc(), calc + unit
@@ -64,9 +30,35 @@ Base.:*(x::Number, y::Calc) = Calc("$x * $(y.expr)")
 Base.:+(x::Unit,   y::Calc) = Calc("$x + $(y.expr)")
 Base.:+(x::Calc,   y::Unit) = Calc("$(x.expr) + $y")
 
+# common css units (ex, ch excluded)
+const px = BareUnit{Unit{:px}}()
+const em = BareUnit{Unit{:em}}()
+const rem = BareUnit{Unit{:rem}}()
+const vh = BareUnit{Unit{:vh}}()
+const vw = BareUnit{Unit{:vw}}()
+const vmin = BareUnit{Unit{:vmin}}()
+const vmax = BareUnit{Unit{:vmax}}()
+const pc = BareUnit{Unit{Symbol("%")}}()
+
+
+# todo: division, substraction?
+
+@show 5px
+
+@show 2px + 2px
+@show 2px + 2.0px
+
+@show 5 * 2px
+@show 5 * 2.0px
+@show 5.0 * 2px
+
+@show 5 * (1px + 2em)
+@show 3.2 * (4.3em + 1px + 3px)
+
 # todo: division, substraction
 
-# @show 1px, 2px, 1px + 2px
-# @show 1px, 2em, 1px + 2em
-# @show 5 * (1px + 2em)
-# @show 3.2 * (4.3em + 1px + 3px)
+
+
+for op in (:+, :-, :*, :/)
+
+end
