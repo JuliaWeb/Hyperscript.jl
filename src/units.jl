5916b1b1b36ff0307e9b07da1cfdc03ff2f84190
Yuri Vishnevsky <yurivish@gmail.com>
2018-02-25 16:44:04 -0500

    some improvements as a result of test-writing

--- src/units.jl
+++ src/units.jl
@@ -5,15 +5,11 @@ Unit{S}(value) where {S} = Unit{S, typeof(value)}(value)
 
 Base.show(io::IO, x::Unit{S}) where {S} = print(io, x.value, S)
 
-# concise scalar * unit construction, e.g. 5px
-struct BareUnit{T} end
-Base.:*(x::T, ::BareUnit{U}) where {U <: Unit, T<:Number} = U{T}(x)
-
-# diagonal dispatch for unit + unit and unit - unit
+# diagonal dispatch for unit + unit, unit - unit
 Base.:+(x::Unit{S}, y::Unit{S}) where {S} = Unit{S}(x.value + y.value)
 Base.:-(x::Unit{S}, y::Unit{S}) where {S} = Unit{S}(x.value - y.value)
 
-# scalar * unit and unit / scalar
+# scalar * unit, unit / scalar
 Base.:*(x::Number, y::U) where {U <: Unit} = U(x + y.value)
 Base.:/(x::U, y::Number) where {U <: Unit} = U(x.value / y)
 
@@ -36,9 +32,13 @@ Base.:+(x::Calc,   y::Unit) = Calc("$(x.expr) + $y")
 Base.:-(x::Unit,   y::Calc) = Calc("$x - $(y.expr)")
 Base.:-(x::Calc,   y::Unit) = Calc("$(x.expr) - $y")
 
-# scalar * calc(), scalar / calc()
+# scalar * calc(), calc() / scalar
 Base.:*(x::Number, y::Calc) = Calc("$x * $(y.expr)")
 Base.:/(x::Calc, y::Number) = Calc("$(x.expr) / $y")
+
+# concise scalar * unit construction, e.g. 5px
+struct BareUnit{T} end
+Base.:*(x::T, ::BareUnit{U}) where {U <: Unit, T<:Number} = U{T}(x)
 
 # common css units (ex, ch excluded)
 const px = BareUnit{Unit{:px}}()
@@ -51,14 +51,11 @@ const vmax = BareUnit{Unit{:vmax}}()
 const pc = BareUnit{Unit{Symbol("%")}}()
 
 # @show 5px
-
 # @show 2px + 2px
 # @show 2px + 2.0px
-
 # @show 5 * 2px
 # @show 5 * 2.0px
 # @show 5.0 * 2px
-
 # @show 5 * (1px + 2em)
 # @show 3.2 * (4.3em + 1px + 3px)
 
