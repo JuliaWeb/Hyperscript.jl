4b4d714e8962ba76f350e91e1366ceede8d15410
Yuri Vishnevsky <yurivish@gmail.com>
2018-04-25 12:22:58 -0400

    remove extra newline

--- src/Hyperscript.jl
+++ src/Hyperscript.jl
@@ -151,7 +151,6 @@ struct RenderContext
 end
 const default_rctx = RenderContext(false, "  ", 0)
 
-
 """
 Wrapper struct for pretty-printing `Node`s: `Pretty(node)`.
 Line feeds are added along with indentation controlled by `indent`.
